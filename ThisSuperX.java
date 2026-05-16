bd80e97f2014b68aee558d1e83a12472886dfe16
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-17 02:01:02 +0530

    Add files via upload

---
+++ ThisSuperX.java
@@ -0,0 +1,42 @@
+class Base
+{
+    public int i;
+    public int j;
+
+    public Base()
+    {
+        System.out.println("Inside Base constructor");
+        this.i=0;
+        this.j=1;
+    }
+
+
+    public void Fun()
+    {
+        System.out.println("Inside Base Fun ");
+    }
+}
+
+class Derived extends Base
+{
+    public int x;
+
+    public Derived()
+    {
+        System.out.println("Inside Derived constructor");
+        this.x=2;
+
+        public void Gun()
+        {
+            System.out.println("Inside Gun of Derived ");
+        }
+    }
+}
+
+class ThisSuperX
+{
+    public static void main(String A[])
+    {
+      System.out.println("Inside main ");
+    }
+}
