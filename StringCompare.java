28d5896153fb53b8721b5d953a1c59e7da1b7d3e
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-21 00:21:52 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,18 @@
+class StringCompare
+{
+    public static void main(String A[])
+    {
+        String S1="Ganesh";
+
+        String S2=new String( "Ganesh");
+
+        if( S1 == S2 )
+        {
+            System.out.println("Strings are equal");
+        }
+        else
+        {
+           System.out.println("Strings are not equal") ;
+        }
+    }
+}
---
+++ StringCompareX.java
@@ -0,0 +1,18 @@
+class StringCompareX
+{
+    public static void main(String A[])
+    {
+        String S1="Ganesh";
+
+        String S2=new String( "Ganesh");
+
+        if( S1.equals(S2))
+        {
+            System.out.println("Strings are equal");
+        }
+        else
+        {
+           System.out.println("Strings are not equal") ;
+        }
+    }
+}
