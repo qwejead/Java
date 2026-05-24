93762f41c5c6fdb7090f8cafd5c1a9f50d95fbae
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-25 00:24:28 +0530

    Add files via upload

---
+++ MultiThreadingLoopJoin.java
@@ -0,0 +1,33 @@
+class Demo extends Thread
+{
+    public void run()
+    {
+        int i=0;
+
+        for(i=1;i<=8;i++)
+        {
+        System.out.println("Inside run of :"+Thread.currentThread().getName()+":"+i);            
+        }
+    }
+}
+class MultiThreadingLoopJoin
+{
+    public static void main(String A[]) throws Exception
+    {
+        System.out.println("main thread is running");
+
+        Demo dobj1=new Demo();
+        Demo dobj2 = new Demo();
+
+        dobj1.setName("FIRST");
+        dobj2.setName("SECOND");
+
+        dobj1.start();
+        dobj2.start();
+
+        dobj1.join();
+        dobj2.join();
+
+        System.out.println("End of main thread");
+    }
+}
