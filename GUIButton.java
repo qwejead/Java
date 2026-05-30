ff0b1d60ed53a8b470ae0f0593e87a800cc2f669
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-31 00:21:54 +0530

    Add files via upload

---
+++ GUIButton.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+class GUIButton
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame("Marvellous PPA");
+        fobj.setSize(400,300);
+
+        JButton bobj=new JButton("YEEEHHHHHHH");
+
+        fobj.add(bobj);//added button
+
+        fobj.setLayout(null);
+
+        fobj.setVisible(true);
+
+        fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
