8bb14db54a31a83ef4d56dad17afc503ce5592e7
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-06-07 01:42:48 +0530

    Add files via upload

---
+++ HashTableDemoXX.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HashTableDemoXX
+{
+    public static void main(String A[])
+    {
+        Hashtable<String, Integer> hobj = new Hashtable<String, Integer>();
+
+        hobj.put("PPA", 27000);
+        hobj.put("LB", 28000);
+        hobj.put("Python", 29000);
+        hobj.put("LB", 28500);
+
+        System.out.println(hobj.get("Python"));
+        System.out.println(hobj.get("PPA"));
+        System.out.println(hobj.get("LB"));
+
+        System.out.println(hobj);
+
+        System.out.println(hobj.keys());
+
+        Enumeration eobj = hobj.keys();
+
+        while(eobj.hasMoreElements())
+        {
+            System.out.println(hobj.get(eobj.nextElement()));
+        }
+    }
+}
