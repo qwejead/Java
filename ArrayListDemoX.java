1f482f54f8a793da006ddda45058ae1568f37b88
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-06-09 03:23:13 +0530

    Add files via upload

---
+++ ArrayListDemoX.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class ArrayListDemoX
+{
+    public static void main(String A[])
+    {  
+        ArrayList <String> aobj=new ArrayList <String> ();
+
+        aobj.add("C++");
+        aobj.add("Java");
+        aobj.add("Python");
+
+        System.out.println(aobj);
+
+    }
+}
