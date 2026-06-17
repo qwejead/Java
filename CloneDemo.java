b899bdec1083386ec0d60f3dcd48a2e6e907a45f
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-06-18 00:34:09 +0530

    Add files via upload

---
+++ CloneDemo.java
@@ -0,0 +1,40 @@
+
+class Student implements Cloneable
+{
+    public String Name;
+    public int Age;
+    public int Marks;
+
+    Student(String A,int B,int C)
+    {
+        this.Name=A;
+        this.Age=B;
+        this.Marks=C;
+    }
+
+    public String toString()
+    {
+        return "Name:"+this.Name+" Age:"+this.Age+" Marks:"+this.Marks;
+    }
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+}
+
+class CloneDemo
+{
+    public static void main(String A[]) throws Exception
+    {
+        Student sobj=new Student("Amit",27,98);
+        Student sobjx=(Student)sobj.clone();
+
+        System.out.println(sobj);
+        System.out.println(sobjx);
+
+        System.out.println(sobj.hashCode());
+        System.out.println(sobjx.hashCode());
+
+    }
+}
