47ff1c74c94f6828ada5cbdc3846cc06724f2f1b
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-09-06 17:31:23 +0530

    Add files via upload

---
+++ program147.java
@@ -0,0 +1,41 @@
+class ArrayX
+{
+    public void Update(int Arr[],int iSize)
+    {
+        int iCnt=0;
+
+        for(iCnt=0;iCnt<iSize;iCnt++)
+        {
+            Arr[iCnt]++;
+        }
+    }
+}
+
+
+class program147
+{
+
+    public static void main(String A[])
+    {
+        int iCnt=0;
+        int Brr[]={10,20,30,40,50};
+        ArrayX aobj=new ArrayX();       //here we created object because ArrayX is non-static object
+
+        System.out.println("Array elements before function :");
+
+        for(iCnt=0;iCnt<5;iCnt++)
+        {
+            System.out.println(Brr[iCnt]);
+        }
+
+        aobj.Update(Brr,5);
+
+        System.out.println("Array elements After function :");
+
+        for(iCnt=0;iCnt<5;iCnt++)
+        {
+            System.out.println(Brr[iCnt]);
+        }
+
+    }
+}
